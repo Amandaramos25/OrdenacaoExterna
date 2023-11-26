d02c057b913bf13d088999cf246821ba55e19977
Amanda Machado <amanda.ramos25@hotmail.com>
2023-11-26 18:23:25 -0300

    Inicio

---
+++ OrdenacaoExterna/src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+
+        System.out.println("-------------------\nDigite qual metodo de ordenação externa deseja fazer: \n\n1. Ordenação por intercalação");
+        int op = sc.nextInt();
+        switch(op){
+            case 1:
+            System.out.println("Ordenação externa por Intercalação: ");
+
+            break;
+        }
+        }
+    }
+}
---
+++ OrdenacaoExterna/src/OrdenarExterno.java
@@ -0,0 +1,5 @@
+public class OrdenarExterno {
+    public void Intercalacao(){
+        
+    }
+}
