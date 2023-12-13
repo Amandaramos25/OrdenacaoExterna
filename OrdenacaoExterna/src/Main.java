cfb0f06fac33448f4e25577380765f662a0c29fa
Amanda Machado <amanda.ramos25@hotmail.com>
2023-12-12 23:01:56 -0300

    at

--- OrdenacaoExterna/src/Main.java
+++ OrdenacaoExterna/src/Main.java
@@ -8,28 +8,39 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         OrdenacaoInterna oi = new OrdenacaoInterna();
-        File arqOrigin = new File("C://Users//Ryan//Documents//GitHub//OrdenacaoExterna//OrdenacaoExterna//arquivos//ordExt_teste.txt");
+        File arqOrigin = new File("C://Users//Amanda//Documents//GitHub//WebIITPI//OrdenacaoExterna//OrdenacaoExterna//arquivos//testeMenor.txt");
         Scanner leitor = new Scanner(arqOrigin);
-        int i = 1;
-        int numGrupo = 1;
-        int left = 0;
-        int right = i-1;
-        while (leitor.hasNextLine()) {
-            String strnumGrupo = "arq"+numGrupo+".txt";
-            File novoArquivo = new File(strnumGrupo);
-            FileWriter escrever = new FileWriter(strnumGrupo);
-            Double numerosDesondenados[] = new Double[i];
-            while (i<=3 && leitor.hasNextLine()) {
+        int i = 0;
+        int numGrupo = 0;
+        int n = 2, left=i, right=n;
+
+        Double numerosDesondenados[] = new Double[n];
+        while (leitor.hasNextLine()) { //enquanto tiver instancias no arquivo original
+            String strnumGrupo = "arq"+numGrupo+".txt"; //nome do arquivo novo
+            File novoArquivo = new File(strnumGrupo); //criando arquivo novo
+            FileWriter escrever = new FileWriter(novoArquivo); //escrever no arquivo
+            
+            while (i<3 && leitor.hasNextLine()) {
                 escrever.write(leitor.nextLine());
+                System.out.println(i);
                 numerosDesondenados[i] = Double.parseDouble(leitor.nextLine());
+                System.out.println(numerosDesondenados[i]);
                 i++;
             }
             //ordenação interna
-            oi.Quicksort(numerosDesondenados, left, right);
+            //oi.Quicksort(numerosDesondenados, left, right);
             escrever.close();
             numGrupo++;
             i=0;
+            clear(numerosDesondenados);
         }
         leitor.close();
     }
+    public static void clear(Double numerosDesondenados[]) {
+
+		for (int i = 0; i < numerosDesondenados.length; i++) {
+
+			numerosDesondenados[i] = null;
+		}
+	}
 }
